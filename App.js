88a6e8dc7ec8e196f88b428f970e13e6ba3b24a6
Makotosoul <minhthien.ng4612@gmail.com>
2024-08-20 12:23:55 +0300

    calculator Thien Nguyen

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [x, setX] = useState("");
+	const [y, setY] = useState("");
+	const [result, setResult] = useState("");
+
+	const plusPressed = () => {
+		const plus = String(Number(x) + Number(y));
+		setResult(plus);
+	};
+	const minusPressed = () => {
+		const minus = String(Number(x) - Number(y));
+		setResult(minus);
+	};
+	return (
+		<View
+			style={{
+				flex: 1,
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<Text style={{ fontSize: 20 }}>Result: {result}</Text>
+			<TextInput
+				style={{ fontSize: 20 }}
+				keyboardType="numeric"
+				returnKeyType="done"
+				placeholder="x here"
+				onChangeText={(text) => setX(text)}
+				value={x}
+			/>
+			<TextInput
+				style={{ fontSize: 20 }}
+				keyboardType="numeric"
+				returnKeyType="done"
+				placeholder="y here"
+				onChangeText={(text) => setY(text)}
+				value={y}
+			/>
+			<View
+				style={{
+					flex: 0,
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "space-around",
+				}}
+			>
+				<Button  title="+" onPress={plusPressed} />
+				<Button title="-" onPress={minusPressed} />
+			</View>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
